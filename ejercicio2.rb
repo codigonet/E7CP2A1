6d2509a0d131309c0b3f6fa420ff7819bed96d59
Cristian Acuña <cas@codigonet.net>
2018-07-26 21:48:45 -0400

    ejercicio 2

---
+++ ejercicio2.rb
@@ -0,0 +1,17 @@
+nombres = ['Violeta', 'Andino', 'Clemente', 'Javiera', 'Paula', 'Pia', 'Ray']
+print "#{nombres}\n\n"
+
+nombres_m = nombres.select { |el| el.length > 5 }
+print "#{nombres_m}\n\n"
+
+nombres_minusculas = nombres.map(&:downcase)
+print "#{nombres_minusculas}\n\n"
+
+nombres_p = nombres.select { |el| el[0] == 'P'}
+print "#{nombres_p}\n\n"
+
+nombres_l = nombres.map(&:length)
+print "#{nombres_l}\n\n"
+
+nombres_v = nombres.map { |el| el.gsub(/[aeiou]/i, '') }
+print "#{nombres_v}\n\n"
